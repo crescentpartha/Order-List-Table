8f770c167f51e80fbfeb024feed704ce3b21dda7
crescentpartha <crescentpartha406@gmail.com>
2023-09-21 12:29:00 +0600

    Implement filter functionality for order_status

--- order-list-table/src/components/Order.js
+++ order-list-table/src/components/Order.js
@@ -1,12 +1,23 @@
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import useOrders from '../hooks/useOrders';
 import DisplayOrder from './OrderDisplay';
 
 const Order = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [orderStatus, setOrderStatue] = useState('');
+    const [productName, setProductName] = useState('');
+    // console.log(orderStatus, productName);
     const [search, setSearch] = useState('');
     // console.log(search);
-    const [orders] = useOrders();
+    const [orders] = useOrders({orderStatus, productName});
     // console.log(orders);
+
+    const onSubmit = (data) => {
+        // console.log(data);
+        setProductName(data?.product_name);
+        setOrderStatue(data?.order_status);
+    }
 
     return (
         <div className='m-10'>
@@ -18,6 +29,47 @@ const Order = () => {
                 placeholder="Search by customer name..."
                 className="input input-bordered input-primary w-full max-w-xs"
             />
+
+            {/* Implement filter functionality for order_status */}
+            <form className='flex flex-row gap-2 my-2 justify-center' onSubmit={handleSubmit(onSubmit)}>
+
+                {/* Order Status */}
+                <div className='text-start'>
+                    <select {...register("order_status", { required: "Select one option" })} defaultValue="" className='select select-primary w-full max-w-xs'>
+                        <option value="">Select Order Status</option>
+                        <option value="Pending">Pending</option>
+                        <option value="Shipped">Shipped</option>
+                        <option value="Delivered">Delivered</option>
+                    </select>
+                    <label className="label">
+                        {errors.gender?.type === 'required' && <span className="label-text-alt text-error">{errors.gender?.message}</span>}
+                    </label>
+                </div>
+
+                {/* Product Name */}
+                <div className='text-start'>
+                    <select {...register("product_name", { required: "Select one option" })} defaultValue="" className='select select-primary w-full max-w-xs'>
+                        <option value="">Select Product Name</option>
+                        <option value="Product A">Product A</option>
+                        <option value="Product B">Product B</option>
+                        <option value="Product C">Product C</option>
+                        <option value="Product D">Product D</option>
+                        <option value="Product E">Product E</option>
+                    </select>
+                    <label className="label">
+                        {errors.type?.type === 'required' && <span className="label-text-alt text-error">{errors.type?.message}</span>}
+                    </label>
+                </div>
+
+                {/* Filter */}
+                <div>
+                    <input
+                        className='btn btn-md btn-primary btn-wide'
+                        type="submit"
+                        value="Filter"
+                    />
+                </div>
+            </form>
 
             <h2 className="text-3xl font-medium font-lobster mb-2 text-secondary text-center uppercase">List of Orders</h2>
 
--- order-list-table/src/hooks/useOrders.js
+++ order-list-table/src/hooks/useOrders.js
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react";
 
-const useOrders = () => {
+const useOrders = ({orderStatus, productName}) => {
     const [orders, setOrders] = useState([]);
+    // console.log(orderStatus, productName);
 
     useEffect(() => {
-        fetch('http://localhost:5000/order')
+        const url = `http://localhost:5000/order?order_status=${orderStatus}&product_name=${productName}`;
+        // console.log(url);
+        fetch(url)
             .then(res => res.json())
             .then(data => setOrders(data));
-    }, [orders]);
+    }, [orders, orderStatus, productName]);
 
     return [orders, setOrders];
 }
